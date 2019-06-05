7794817e8f98e12b89e0e228ca10aee4807e2d1b
jasonhsieh <jasonhsieh@ghtinc.com>
2019-06-05 16:07:47 +0800

    gzip csv parser works but not insert into clickhouse

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:18.04
-RUN apt-get update
-RUN apt-get install -y vim curl
-
--- start-logstash.sh
+++ start-logstash.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
+# if permission denied use below command
+# sudo chmod -R 757 ./logstash-logs
 
-docker run --rm -it \
---name logstsah \
+sudo docker run --rm -it \
+ --privileged=true -i \
+--name logstash \
+-v $(pwd)/sample-csv/:/sample-csv/ \
+-v $(pwd)/logstash-logs/:/data/ \
 -v $(pwd)/logstash.conf:/usr/share/logstash/pipeline/logstash.conf \
 -v $(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml \
 -v $(pwd)/logstash-output-clickhouse-0.1.0.gem:/usr/share/logstash/bin/logstash-output-clickhouse-0.1.0.gem \
 -p 5044:5044 \
-docker.elastic.co/logstash/logstash:5.6.3 /bin/bash
+docker.elastic.co/logstash/logstash:7.1.1 /bin/bash
+
+# permission error 
+# https://stackoverflow.com/questions/49414167/error-permission-denied-usr-share-logstash-sincedb-sincedb
